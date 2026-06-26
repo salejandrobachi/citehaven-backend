316aa67322ecc98ee9392709fd796a6e648ddd69
bachi0725 <serbalz@hotmail.com>
2026-06-25 21:39:44 -0500

    Setup inicial: Apollo Server + TypeScript funcionando

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'CiteHaven backend funcionando 🎉',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Servidor listo en: ${url}`);
